60242eda93e2505f6172c507a0925859cf1381d4
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-06 01:03:21 +0600

    admin login route added

--- devmohib-client-/src/Pages/Admin/AdminLogin.jsx
+++ devmohib-client-/src/Pages/Admin/AdminLogin.jsx
@@ -1,14 +1,11 @@
 
 const AdminLogin = () => {
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="flex h-screen items-center justify-center">
             <div className="relative flex flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
-                <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                    Sign Up
+                <h4 className="block text-center font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                    Sign in only admin
                 </h4>
-                <p className="mt-1 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-                    Enter your details to register.
-                </p>
                 <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="mb-4 flex flex-col gap-6">
                         <div className="relative h-11 w-full min-w-[200px]">
@@ -30,43 +27,11 @@ const AdminLogin = () => {
                             </label>
                         </div>
                     </div>
-                    <div className="inline-flex items-center">
-                        <label className="relative -ml-2.5 flex cursor-pointer items-center rounded-full p-3" htmlFor="checkbox" data-ripple-dark="true">
-                            <input type="checkbox" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10" id="checkbox" />
-                            <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth={1}>
-                                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                </svg>
-                            </span>
-                        </label>
-                        <label className="mt-px cursor-pointer select-none font-light text-gray-700" htmlFor="checkbox">
-                            <p className="flex items-center font-sans text-sm font-normal leading-normal text-gray-700 antialiased">
-                                I agree the
-                                <a className="font-semibold transition-colors hover:text-pink-500" href="#">
-                                    &nbsp;Terms and Conditions
-                                </a>
-                            </p>
-                        </label>
-                    </div>
                     <button className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" data-ripple-light="true">
-                        Register
+                        Sign In
                     </button>
-                    <p className="mt-4 block text-center font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-                        Already have an account?
-                        <a className="font-semibold text-pink-500 transition-colors hover:text-blue-700" href="#">
-                            Sign In
-                        </a>
-                    </p>
                 </form>
-                <div className="w-full pt-5 px-4 mb-8 items-center ">
-                    <div className="text-sm text-gray-700 py-1">
-                        Made with <a className="text-gray-700 font-semibold" href="https://www.material-tailwind.com/docs/html/form?ref=tailwindcomponents" target="_blank">Material Tailwind</a> by <a href="https://www.creative-tim.com?ref=tailwindcomponents" className="text-gray-700 font-semibold" target="_blank"> Creative Tim</a>.
-                    </div>
-                </div>
             </div>
-            {/* stylesheet */}
-            <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
-            {/* from cdn */}
         </div>
     );
 };
