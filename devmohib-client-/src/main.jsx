df761c110dccd0d3f690417ab806c6c4e5e003db
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-17 01:14:59 +0600

    try to deploy 15

--- devmohib-client-/src/main.jsx
+++ devmohib-client-/src/main.jsx
@@ -6,17 +6,24 @@ import Layout from "./Layout/Layout.jsx";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage.jsx";
 import AdminLogin from "./Pages/Admin/AdminLogin.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "admin-login",
+      element: <AdminLogin />,
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,    
-  },
-  {
-    path: "admin-login",
-    element: <AdminLogin />,   
-  },
-]);
+    future: {
+      v7_startTransition: true,
+    },
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
