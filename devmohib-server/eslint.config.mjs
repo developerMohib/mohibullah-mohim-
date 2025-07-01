08e08ccd5bfbb9b15d7222ead84a82e5e6fa9322
developerMohib <mohibullahmohim2020@gmail.com>
2025-07-01 19:10:42 +0600

    add server with ts

---
+++ devmohib-server/eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_modules", "dist"],
+     rules: {
+    "no-unused-vars": "error",
+    "no-undef": "error",
+    "prefer-const": "error",
+    "no-console": "warn"
+  },
+  },
+]);
